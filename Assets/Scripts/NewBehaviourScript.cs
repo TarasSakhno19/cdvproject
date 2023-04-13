afcf69ac89b712797a6bd35f7a6102da34f64bb2
TarasSakhno19 <tsakhno@edu.cdv.pl>
2023-04-13 16:31:17 +0200

    Finished

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
-{
+{ 
+    public Rigidbody2D rb2D;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.velocity = new Vector2(Random.Range(-1f, 1f), Random.Range (-1f, 1f));
     }
 
     // Update is called once per frame
